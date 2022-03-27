4d292407f63738036d65071c0e444f8c7ba39f9e
kebin <kebinImports@gmail.com>
2022-03-27 05:33:03 -0700

    Renamed PANIC FIX ALL to PANIC HARD RESET ALL

--- Editor/Scripts/Items/400-430_Utilities/430_PHRA.cs
+++ Editor/Scripts/Items/400-430_Utilities/430_PHRA.cs
@@ -4,14 +4,14 @@ using UnityEditor;
 
 public partial class kebinImports : MonoBehaviour
 {
-    [MenuItem("kebinImports/PANIC FIX ALL", false, 430)]
-    private static void PFA()
+    [MenuItem("kebinImports/PANIC HARD RESET ALL", false, 430)]
+    private static void PHRA()
     {
         if (hideWarnings == false)
         {
             if (EditorUtility.DisplayDialog(
                 "WARNING!",
-                "WARNING!\n\nThis will most likely fix any errors you have. However, it does require a restart of Unity to do so. Please relaunch Unity a few seconds after it closes.\n\nDo you wish to continue?",
+                "WARNING!\n\nThis will most likely fix any errors you have. It is a Hard Reset to the UnityEditor Appliation's Editor Preferences while also Fixing Scripting Define Symbols. It is meant to be used when you are in a PANIC and cannot figure out issues with the project. However, it does require a restart of Unity to do so. Please relaunch Unity a few seconds after it closes.\n\nDo you wish to continue?",
                 "Yes",
                 "No"
             ) == false) return;
